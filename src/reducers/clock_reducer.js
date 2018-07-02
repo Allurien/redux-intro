df5030335d7eb19621256182dee86ec0634ffc2f
Steffany <kielen@gmail.com>
2018-07-02 12:59:48 -0700

    setup redux structure

---
+++ src/reducers/clock_reducer.js
@@ -0,0 +1,11 @@
+
+const DEFAULT_STATE = {
+    time: null
+};
+
+export default (state = DEFAULT_STATE, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
